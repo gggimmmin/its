922cb9c5b227f02a2b778c07309e629436ea3921
ji min <142903244+gggimmmin@users.noreply.github.com>
2024-05-27 19:11:23 +0900

    [Feat] firebase 설치 및 초기 세팅

---
+++ src/firebase.ts
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC6TDwjD7Jew5KheRyBBiARzkLsrNH8pQs",
+  authDomain: "it-s-c9194.firebaseapp.com",
+  projectId: "it-s-c9194",
+  storageBucket: "it-s-c9194.appspot.com",
+  messagingSenderId: "971405342487",
+  appId: "1:971405342487:web:f89ddfb1aa06883f242952",
+  measurementId: "G-S7HNLM7BFM",
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
