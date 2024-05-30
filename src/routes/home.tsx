c3ca63d1e5bc328d2bf79af5b7e0f5dec6b817be
ji min <142903244+gggimmmin@users.noreply.github.com>
2024-05-30 18:20:44 +0900

    [Feat] Post From 구현

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,3 +1,12 @@
+import styled from "styled-components";
+import PostUploadForm from "../components/post-upload-form";
+
 export default function Home() {
-  return <h1>Home</h1>;
+  return (
+    <Wrapper>
+      <PostUploadForm />
+    </Wrapper>
+  );
 }
+
+const Wrapper = styled.div``;
