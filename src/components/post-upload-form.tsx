236e6d52624134d14b796d86c53c599f6d8b4944
ji min <142903244+gggimmmin@users.noreply.github.com>
2024-05-31 20:07:00 +0900

    [Feat] firestore DB 에 post 업로드 구현

--- src/components/post-upload-form.tsx
+++ src/components/post-upload-form.tsx
@@ -1,5 +1,7 @@
+import { addDoc, collection } from "firebase/firestore";
 import React, { useState } from "react";
 import styled from "styled-components";
+import { auth, db } from "../firebase";
 
 export default function PostUploadForm() {
   const [isLoading, setLoading] = useState(false);
@@ -14,8 +16,26 @@ export default function PostUploadForm() {
       setFile(files[0]);
     }
   };
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const user = auth.currentUser;
+    if (!user || isLoading || post === "" || post.length > 200) return;
+    try {
+      setLoading(true);
+      await addDoc(collection(db, "posts"), {
+        post,
+        createdAt: Date.now(),
+        username: user.displayName || "Anonymous",
+        userId: user.uid,
+      });
+    } catch (e) {
+      console.log(e);
+    } finally {
+      setLoading(false);
+    }
+  };
   return (
-    <Form>
+    <Form onSubmit={onSubmit}>
       <TextArea
         rows={5}
         maxLength={200}
